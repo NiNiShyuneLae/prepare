fd084ff5a8ad8125fa778542e022622216a140ad
NiNiShyuneLae <heathersan865@gmail.com>
2023-06-02 12:59:12 +0630

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Home from './pages/Home'
+import Login from './pages/Login'
 
 const App = () => {
   return (
     <div>
       <h1>This is demo of branch and merge</h1>
       <Home/>
+      <Login/>
     </div>
   )
 }
